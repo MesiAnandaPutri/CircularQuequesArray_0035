a6c84b18b1bd4584d8992251d872b4034d80f61e
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-30 18:57:33 +0700

    cek apakah kosong

--- strukturQueue/strukturQueue.cpp
+++ strukturQueue/strukturQueue.cpp
@@ -28,3 +28,14 @@ public:
 			FRONT = 0;
 			REAR = 0;
 		}
+
+		// cek apakah kosong
+		else {
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+
+		}
+		queue_array[REAR] = num;
+	}
