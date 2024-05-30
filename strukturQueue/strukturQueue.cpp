8d68285be82fdb672166c5cb45e19272a2b56e4e
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-30 18:59:30 +0700

    jika rear berada di posisi terakhir array, kembali ke awal array

--- strukturQueue/strukturQueue.cpp
+++ strukturQueue/strukturQueue.cpp
@@ -53,4 +53,11 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
-		
+		else {
+			// jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+	}
