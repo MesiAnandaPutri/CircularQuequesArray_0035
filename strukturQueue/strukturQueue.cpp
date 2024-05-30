81214815928ebd76efad463065a2fd2fe8f9e1c8
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-30 19:00:59 +0700

    iterasi dari front hingga rear

--- strukturQueue/strukturQueue.cpp
+++ strukturQueue/strukturQueue.cpp
@@ -72,3 +72,12 @@ public:
 			return;
 		}
 		cout << "\nElemen in the queue are...\n";
+
+		// jika FRONT_position << REAR_position, iterasi dri FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
